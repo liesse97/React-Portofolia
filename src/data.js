31f421d143fec02304a2f5ab2c934313d958724d
Liesse Ishimwe <ishimwelieish@gmail.com>
2021-11-24 13:46:14 +0100

    'filter-button'

---
+++ src/data.js
@@ -0,0 +1,53 @@
+
+export default [
+    {
+        id:1,
+        kind:'code',
+        name: "Portfolia",
+        technique:"React js, React particle js",
+        picture:"/images/potorofolia bild.png",
+        codeLink:"https://github.com/liesse97/React-Portofolia.git",
+        liveLink:"https://liesse.netlify.app/"
+    },
+
+ {
+        id:2,
+        kind:'code',
+        name: "Chat Application",
+        technique:'React JS, Firebase, Chat Engine',
+        picture:"images/Mychat.png",
+        codeLink:" https://github.com/liesse97/Chat-react.git",
+        liveLink:"https://mychat2.netlify.app/"
+    },
+
+     {
+        id:3,
+        kind:'code',
+        name: 'Music player',
+        technique:'HTML/CSS JS',
+        picture:"images/music.png",
+        codeLink:"https://github.com/liesse97/Music-player.git",
+        liveLink:"https://music-player2.netlify.app/" 
+    },
+     {
+        id:4,
+        kind:'wordpress',
+        name: 'Föbu-sverige',
+        technique:'Wordpress',
+        picture:"images/föbu-sverige.png",
+        liveLink:"https://www.fobu-sverige.se/",
+    },
+    //  {
+    //     id:5,
+    //     kind:'wordpress',
+    //     name: 'Bellhair&Beauty',
+    //     technique:'Wordpress , E-commerce',
+    //     picture:"images/bellhair.png",
+    //     liveLink:"coming soon",
+    // },
+
+
+
+
+]
+;
